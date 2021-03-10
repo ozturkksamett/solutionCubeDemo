1c72717dde255ebaadb4af543f808c2a6113e3a3
Solution <Solution@ETHEMMSI>
2021-03-11 02:12:20 +0300

    execute işlemi methodlara bölme

--- solutioncubedemo/src/main/java/com/example/demo/controller/DenemeController.java
+++ solutioncubedemo/src/main/java/com/example/demo/controller/DenemeController.java
@@ -1,6 +1,5 @@
 package com.example.demo.controller;
 
-
 import java.io.IOException;
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
@@ -53,18 +52,14 @@ import com.squareup.okhttp.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
-
-
 @RestController
 public class DenemeController {
 
 	@Autowired
 	private final DenemeRepository repository;
-	@Autowired 
+	@Autowired
 	private Scheduler scheduler;
 	private static final Logger logger = LoggerFactory.getLogger(DenemeController.class);
-
 
 	DenemeController(DenemeRepository repository) {
 		this.repository = repository;
@@ -75,39 +70,34 @@ public class DenemeController {
 		return "Hello World!";
 	}
 
-	
 	@RequestMapping("/ekle")
 	public void addNewValue() throws UnirestException, IOException {
 		List<Deneme> denemeler = new ArrayList<Deneme>();
-		
-		
+
 		OkHttpClient client = new OkHttpClient();
 
 		Request request = new Request.Builder()
-		  .url("https://api.triomobil.com/facility/v1/sensors?_sortBy=label&_sortOrder=ASC&_page=1&_perPage=20")
-		  .get()
-		  .addHeader("authorization", "971b8df0-444d-4115-b46d-7ee840ac97b6")
-		  .build();
+				.url("https://api.triomobil.com/facility/v1/sensors?_sortBy=label&_sortOrder=ASC&_page=1&_perPage=20")
+				.get().addHeader("authorization", "971b8df0-444d-4115-b46d-7ee840ac97b6").build();
 		Response response = client.newCall(request).execute();
-		
+
 		String jsonData = response.body().string();
 		System.out.println(jsonData);
-		
+
 		JSONArray jsonArray = new JSONArray(jsonData);
 
-		for (int i = 0; i < jsonArray.length(); i++)
-		{
+		for (int i = 0; i < jsonArray.length(); i++) {
 			Deneme deneme = new Deneme();
-		    String post_id = jsonArray.getJSONObject(i).getString("_id");
-		    deneme.setKolon1(post_id);
-		    denemeler.add(deneme);
-		    System.out.println(post_id);
+			String post_id = jsonArray.getJSONObject(i).getString("_id");
+			deneme.setKolon1(post_id);
+			denemeler.add(deneme);
+			System.out.println(post_id);
 		}
 
 		repository.saveAll(denemeler);
-	
+
 	}
-	
+
 	@GetMapping("/denemeler")
 	List<Deneme> all() {
 		return repository.findAll();
@@ -141,59 +131,60 @@ public class DenemeController {
 	void deleteDeneme(@PathVariable Long id) {
 		repository.deleteById(id);
 	}
-	
-	
-	@PostMapping("/scheduleDeneme")
-	public ResponseEntity<ScheculedDenemeResponse> scheduleDeneme(@Valid @RequestBody ScheculedDenemeRequest scheculedDenemeRequest){
 
-		
+	@PostMapping("/scheduleDeneme")
+	public ResponseEntity<ScheculedDenemeResponse> scheduleDeneme(
+			@Valid @RequestBody ScheculedDenemeRequest scheculedDenemeRequest) {
+
 		Calendar c = Calendar.getInstance();
-		
-		 System.out.println("datetime alındı"+scheculedDenemeRequest.getDateTime());
-		 
-		//get current TimeZone using
+
+		System.out.println("datetime alındı" + scheculedDenemeRequest.getDateTime());
+
 		TimeZone tz = c.getTimeZone();
 
 		try {
-			 System.out.println("ZoneId alındı"+tz.toZoneId());
-			 
+			System.out.println("ZoneId alındı" + tz.toZoneId());
+
 			ZonedDateTime dateTime = ZonedDateTime.of(scheculedDenemeRequest.getDateTime(), tz.toZoneId());
-			System.out.println("dateTime "+dateTime);
-			
+			System.out.println("dateTime " + dateTime);
+
 			if (dateTime.isBefore(ZonedDateTime.now())) {
-				ScheculedDenemeResponse sheculedDenemeResponse = new ScheculedDenemeResponse(false, "dateTime must be after current time");
+				ScheculedDenemeResponse sheculedDenemeResponse = new ScheculedDenemeResponse(false,
+						"dateTime must be after current time");
 				return ResponseEntity.badRequest().body(sheculedDenemeResponse);
-			}	
+			}
 			int interval = scheculedDenemeRequest.getScheculeInterval();
 			JobDetail jobDetail = buildJobDetail(scheculedDenemeRequest);
-			Trigger trigger = buildJobTrigger(jobDetail, dateTime,interval);
+			Trigger trigger = buildJobTrigger(jobDetail, dateTime, interval);
 			scheduler.scheduleJob(jobDetail, trigger);
-			ScheculedDenemeResponse scheculedDenemeResponse = new ScheculedDenemeResponse(true, jobDetail.getKey().getName(),
-					jobDetail.getKey().getGroup(), "Email Scheduled Successfully!");
+			ScheculedDenemeResponse scheculedDenemeResponse = new ScheculedDenemeResponse(true,
+					jobDetail.getKey().getName(), jobDetail.getKey().getGroup(), "SolutionCube job started Successfully!");
 			return ResponseEntity.ok(scheculedDenemeResponse);
 		} catch (SchedulerException ex) {
 			logger.error("Error scheduling email", ex);
-			ScheculedDenemeResponse scheculedDenemeResponse = new ScheculedDenemeResponse(false, "Error scheduling email. Please try later!");
+			ScheculedDenemeResponse scheculedDenemeResponse = new ScheculedDenemeResponse(false,
+					"Error scheduling email. Please try later!");
 			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(scheculedDenemeResponse);
 		}
 
 	}
-	
-	
+
 	private JobDetail buildJobDetail(ScheculedDenemeRequest scheculedDenemeRequest) {
 		JobDataMap jobDataMap = new JobDataMap();
-		/*jobDataMap.put("email", scheduleEmailRequest.getEmail());
-		jobDataMap.put("subject", scheduleEmailRequest.getSubject());
-		jobDataMap.put("body", scheduleEmailRequest.getBody());*/
+		/*
+		 * jobDataMap.put("email", scheduleEmailRequest.getEmail());
+		 * jobDataMap.put("subject", scheduleEmailRequest.getSubject());
+		 * jobDataMap.put("body", scheduleEmailRequest.getBody());
+		 */
 		return JobBuilder.newJob(DenemeJob.class).withIdentity(UUID.randomUUID().toString(), "email-jobs")
 				.withDescription("Send Email Job").usingJobData(jobDataMap).storeDurably().build();
 	}
-	
+
 	private Trigger buildJobTrigger(JobDetail jobDetail, ZonedDateTime startAt, int interval) {
 		return TriggerBuilder.newTrigger().forJob(jobDetail)
-				.withIdentity(jobDetail.getKey().getName(), "email-triggers").withDescription("Send Email Trigger")
+				.withIdentity(jobDetail.getKey().getName(), "SolutionCube insert Data").withDescription("SolutionCube tablolarını besler")
 				.startAt(Date.from(startAt.toInstant()))
-				.withSchedule(SimpleScheduleBuilder.simpleSchedule().repeatMinutelyForever(interval)).build();
+				.withSchedule(SimpleScheduleBuilder.simpleSchedule().repeatHourlyForever(24)).build();
 	}
 
 }
--- solutioncubedemo/src/main/java/com/example/demo/job/DenemeJob.java
+++ solutioncubedemo/src/main/java/com/example/demo/job/DenemeJob.java
@@ -25,54 +25,21 @@ import com.squareup.okhttp.Response;
 @Component
 public class DenemeJob extends QuartzJobBean  {
 	
-	
-	private static final Logger logger = LoggerFactory.getLogger(DenemeJob.class);
+	/*
+	 * todo 40 tane repository tanımı olacak
+	 * 40 tane dto<list> tanımı olacak
+	 * com.eample.demo.dao içerisnde 40 tane dto tanımı 40 tane repository tanımı yapılacak eklenecek
+	 */
 
 	@Autowired
-	private DenemeRepository repository;
-
-
+	DenemeSC denemeSC;
 
 	@Override
 	protected void executeInternal (JobExecutionContext jobExecutionContext) throws JobExecutionException {
 		
-		List<Deneme> denemeler = new ArrayList<Deneme>();
+		//todo 40 tane rest çağrımı yapılacak (40 method çağrılacak)
+		denemeSC.execute();
 		
-		OkHttpClient client = new OkHttpClient();
-
-		Request request = new Request.Builder()
-		  .url("https://api.triomobil.com/facility/v1/sensors?_sortBy=label&_sortOrder=ASC&_page=1&_perPage=20")
-		  .get()
-		  .addHeader("authorization", "971b8df0-444d-4115-b46d-7ee840ac97b6")
-		  .build();
-		Response response;
-		String jsonData;
-		
-		try {
-			response = client.newCall(request).execute();
-			jsonData = response.body().string();
-			JSONArray jsonArray = new JSONArray(jsonData);
-			
-			for (int i = 0; i < jsonArray.length(); i++)
-			{
-				Deneme deneme = new Deneme();
-			    String post_id = jsonArray.getJSONObject(i).getString("_id");
-			    deneme.setKolon1(post_id);
-			    denemeler.add(deneme);
-			    System.out.println(post_id);
-			}
-			System.out.println(jsonData);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		repository.saveAll(denemeler);
-		
-		logger.info("Executing Job with key {}", jobExecutionContext.getJobDetail().getKey());
-		/*JobDataMap jobDataMap = jobExecutionContext.getMergedJobDataMap();
-		String subject = jobDataMap.getString("subject");*/
-
 	}
 
 }
